a6802d466d70ab5ab69e7384d5d487c910041ad1
VICTORVICKIE <s.vickie14@gmail.com>
2023-05-14 19:47:04 +0530

    added useful constants

--- src/lib/Stores.ts
+++ src/lib/Stores.ts
@@ -1,5 +1,16 @@
 import type { SvelteComponent } from 'svelte';
-import { writable } from 'svelte/store';
+import { derived, writable } from 'svelte/store';
 
 export const ids = writable<number[]>([0]);
 export const cells = writable<SvelteComponent[]>([]);
+export const active_cell_idx = writable<number>(0);
+// make a derived store based on idx and cells
+export const active_cell = derived(
+    [active_cell_idx, cells],
+    ([$active_cell_idx, $cells]) => $cells[$active_cell_idx]
+);
+
+export const sync = derived([ids, cells], ([$ids, $cells]) => $ids.length === $cells.length);
+
+export const cell_separator = '\n--Cell--Separator--\n' as const;
+export const app_name = 'SQLite-Notebook' as const;
