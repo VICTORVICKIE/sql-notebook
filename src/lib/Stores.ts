f92b9f10e8fa20e4764f5b7fc7d20aa3c9da04f0
VICTORVICKIE <s.vickie14@gmail.com>
2023-05-17 09:31:46 +0530

    removed old approach and added new linked list

--- src/lib/Stores.ts
+++ src/lib/Stores.ts
@@ -1,17 +1,17 @@
-import type { SvelteComponent } from 'svelte';
-import { derived, writable } from 'svelte/store';
-
-export const ids = writable<number[]>([0]);
-export const cells = writable<SvelteComponent[]>([]);
-export const active_cell_idx = writable<number>(0);
-// make a derived store based on idx and cells
-export const active_cell = derived(
-    [active_cell_idx, cells],
-    ([$active_cell_idx, $cells]) => $cells[$active_cell_idx]
-);
-
-export const sync = derived([ids, cells], ([$ids, $cells]) => $ids.length === $cells.length);
-export const populated = writable<boolean>(false);
+import { writable } from 'svelte/store';
+import type Cell from './Cell.svelte';
+import { DoublyLinkedList } from './DoublyLinkedList';
+import type TextArea from './TextArea.svelte';
 
 export const cell_separator = '\n--Cell--Separator--\n' as const;
 export const app_name = 'SQLite-Notebook' as const;
+
+export interface ICell {
+    component?: Cell,
+    init: boolean,
+    id?: number,
+    idx?: number,
+    textarea?: TextArea
+};
+
+export const notebook = writable(new DoublyLinkedList<ICell>())
